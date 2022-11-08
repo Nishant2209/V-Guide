23ed841121a1e8a64065a05eec447c142ae643bc
Nishant Kumar Singh <nishant220902@gmail.com>
2022-11-08 23:08:47 +0530

    removed dashboard.css file

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,16 +1,13 @@
 import Featured from "../../Components/Featured/Featured";
 import Header from "../../Components/Header/Header";
 import MailList from "../../Components/Mail_List/MailList";
-import "./dashboard.css";
 
 const Dashboard = () => {
   return (
     <div>
       <Header />
-      <div className="homeContainer">
-        <Featured />
-        <MailList />
-      </div>
+      <Featured />
+      <MailList />
     </div>
   );
 };
