1e03bf03f9251eccb35711de04bbd18a53643774
Nishant Kumar Singh <nishant220902@gmail.com>
2022-11-08 15:05:12 +0530

    added new colour

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,9 +20,10 @@ module.exports = {
         lightgreen: "#9fefdbb5",
         darkBlue: "hsl(228,39%,23%)",
         darkGrayishBlue: "hsl(227,12%,61%)",
-        veryDarkBlue: "hsl(233,12%,13%)",
+        veryDarkBlue: "#0C2D4A",
         veryPaleRed: "hsl(13,100%,96%)",
         veryLightGray: "hsl(0,0%,98%)",
+        lightGray: "#cbcddc",
       },
       fontFamily: {
         primary: ["Montserrat", "sans-serif"],
