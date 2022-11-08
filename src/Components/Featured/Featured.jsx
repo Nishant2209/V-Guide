bf189e913d63103973f7ccd477f3887171c41ab8
Nishant Kumar Singh <nishant220902@gmail.com>
2022-11-08 23:08:23 +0530

    removed features.css and modified style of featured

--- src/Components/Featured/Featured.jsx
+++ src/Components/Featured/Featured.jsx
@@ -1,80 +1,76 @@
-import "./featured.css";
+import { FaReact } from "react-icons/fa";
+import { SiAndroidstudio } from "react-icons/si";
 
 const Featured = () => {
   return (
-    <div className="featured">
-      <div className="featuredItem">
-        <img
-          src="https://www.reachfirst.com/wp-content/uploads/2018/08/Web-Development.jpg"
-          alt=""
-          className="featuredImg"
-        />
-        <div className="featuredTitles">
-          <h1>Web Development</h1>
-          <h2>12 Mentors</h2>
+    <div className="flex flex-col justify-center items-center my-20 text-veryDarkBlue t-shadow">
+      <hr className="bg-veryDarkBlue h-[2px] w-[90%]" />
+      <div className="text-6xl font-extrabold my-5">Available Mentors</div>
+      <p className="headerDesc font-bold text-xl w-1/3 text-center my-5 leading-9">
+        Get the guidance from the mentors around you. who are accessible and
+        friendly .
+      </p>
+      <div className="featured grid grid-cols-3 gap-5 container max-w-5xl my-5">
+        <div className="featuredItem flex flex-col justify-center items-center shadow-class w-3/4 rounded-3xl p-8 mx-auto">
+          <FaReact className="text-8xl mb-3" />
+          <div className="featuredTitles space-y-2">
+            <div className="text-center text-4xl font-extrabold">12 +</div>
+            <div className="text-center text-lg font-semibold">
+              Web Development Mentors
+            </div>
+          </div>
+          <div className="featuredBg"></div>
         </div>
-        <div className="featuredBg"></div>
-      </div>
-      
-      <div className="featuredItem">
-        <img
-          src="https://www.w2ssolutions.com/blog/wp-content/uploads/2019/07/ios-app-development-india.png"
-          alt=""
-          className="featuredImg"
-        />
-        <div className="featuredTitles">
-          <h1>IOS Development</h1>
-          <h2>5 Mentors</h2>
+        <div className="featuredItem flex flex-col justify-center items-center shadow-class w-3/4 rounded-3xl p-8 mx-auto">
+          <FaReact className="text-8xl mb-3" />
+          <div className="featuredTitles space-y-2">
+            <div className="text-center text-4xl font-extrabold">12 +</div>
+            <div className="text-center text-lg font-semibold">
+              Web Development Mentors
+            </div>
+          </div>
+          <div className="featuredBg"></div>
         </div>
-        <div className="featuredBg"></div>
-      </div>
-      <div className="featuredItem">
-        <img
-          src="https://reliasoftware.com/static/9ce8aa204a21e69716018fee472b042c/the-complete-guide-to-mobile-app-development-2021.png"
-          alt=""
-          className="featuredImg"
-        />
-        <div className="featuredTitles">
-          <h1>App Development</h1>
-          <h2>10 Mentors</h2>
+        <div className="featuredItem flex flex-col justify-center items-center shadow-class w-3/4 rounded-3xl p-8 mx-auto">
+          <FaReact className="text-8xl mb-3" />
+          <div className="featuredTitles space-y-2">
+            <div className="text-center text-4xl font-extrabold">12 +</div>
+            <div className="text-center text-lg font-semibold">
+              Web Development Mentors
+            </div>
+          </div>
+          <div className="featuredBg"></div>
         </div>
-        <div className="featuredBg"></div>
-      </div>
-      <div className="featuredItem">
-        <img
-          src="https://repository-images.githubusercontent.com/403817624/3d10f761-1027-4d0a-9906-48361e466d87"
-          alt=""
-          className="featuredImg"
-        />
-        <div className="featuredTitles">
-          <h1>DSA</h1>
-          <h2>20 Mentors</h2>
+        <div className="featuredItem flex flex-col justify-center items-center shadow-class w-3/4 rounded-3xl p-8 mx-auto">
+          <SiAndroidstudio className="text-8xl mb-3" />
+          <div className="featuredTitles space-y-2">
+            <div className="text-center text-4xl font-extrabold">12 +</div>
+            <div className="text-center text-lg font-semibold">
+              Web Development Mentors
+            </div>
+          </div>
+          <div className="featuredBg"></div>
         </div>
-        <div className="featuredBg"></div>
-      </div>
-      <div className="featuredItem">
-        <img
-          src="https://www.perfecto.io/sites/default/files/image/2020-09/social-blog-classifying-ai-ml-tools-2.jpg"
-          alt=""
-          className="featuredImg"
-        />
-        <div className="featuredTitles">
-          <h1>AI/ML</h1>
-          <h2>5 Mentors</h2>
+        <div className="featuredItem flex flex-col justify-center items-center shadow-class w-3/4 rounded-3xl p-8 mx-auto">
+          <SiAndroidstudio className="text-8xl mb-3" />
+          <div className="featuredTitles space-y-2">
+            <div className="text-center text-4xl font-extrabold">12 +</div>
+            <div className="text-center text-lg font-semibold">
+              Web Development Mentors
+            </div>
+          </div>
+          <div className="featuredBg"></div>
         </div>
-        <div className="featuredBg"></div>
-      </div>
-      <div className="featuredItem">
-        <img
-          src="https://www.patriotsoftware.com/wp-content/uploads/2022/01/what-is-blockchain-1.jpg"
-          alt=""
-          className="featuredImg"
-        />
-        <div className="featuredTitles">
-          <h1>Blockchain</h1>
-          <h2>3 Mentors</h2>
+        <div className="featuredItem flex flex-col justify-center items-center shadow-class w-3/4 rounded-3xl p-8 mx-auto">
+          <SiAndroidstudio className="text-8xl mb-3" />
+          <div className="featuredTitles space-y-2">
+            <div className="text-center text-4xl font-extrabold">12 +</div>
+            <div className="text-center text-lg font-semibold">
+              Web Development Mentors
+            </div>
+          </div>
+          <div className="featuredBg"></div>
         </div>
-        <div className="featuredBg"></div>
       </div>
     </div>
   );
