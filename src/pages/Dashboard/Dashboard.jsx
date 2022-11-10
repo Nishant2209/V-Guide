bba4b5faac50dbbe729deb574807e53d3bdc6767
Nishant Kumar Singh <nishant220902@gmail.com>
2022-11-10 20:22:12 +0530

    changed the bg colour of body to white

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,6 +1,7 @@
 import Featured from "../../Components/Featured/Featured";
 import Header from "../../Components/Header/Header";
 import MailList from "../../Components/Mail_List/MailList";
+import "./Dashboard.css";
 
 const Dashboard = () => {
   return (
