fe1e0c49a2bffb5e1cf08ec3ca20d7d69b349413
Nishant Kumar Singh <nishant220902@gmail.com>
2022-11-10 20:24:37 +0530

    removed navbar import

--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -1,5 +1,4 @@
 import "./list.css";
-import Navbar from "../../Components/Navbar_Dash/Navbar";
 import Header from "../../Components/Header/Header";
 import SearchItem from "../../Components/Search_Item/SearchItem";
 import MailList from "../../Components/Mail_List/MailList";
