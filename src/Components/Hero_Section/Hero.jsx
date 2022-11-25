65e5ab87afaffb38d7ddfbe60fd24ee98ddb214f
Nishant Kumar Singh <nishant220902@gmail.com>
2022-11-25 16:22:45 +0530

    changed the ui

--- src/Components/Hero_Section/Hero.jsx
+++ src/Components/Hero_Section/Hero.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Navbar from "../Navbar_Land/Navbar";
 import { useNavigate } from "react-router-dom";
 
 function Hero() {
@@ -12,8 +13,9 @@ function Hero() {
   };
 
   return (
-    <section className="Hero_Section" id="Hero_Section">
-      <div className="flex flex-col-reverse md:flex-row mx-auto pb-20 md:pb-0 items-center md:px-28 md:mt-20">
+    <section className="Hero_Section bg-gradient-to-b from-[#b4b7cd] via-white to-[#f0d0cb]" id="Hero_Section">
+      <Navbar />
+      <div className="flex flex-col-reverse md:flex-row mx-auto pb-20 md:pb-0 items-center md:px-28 md:mt-10">
         {/* Left */}
         <div className="content flex flex-col space-y-10 md:w-1/2">
           <h1 className="text-4xl text-center leading-tight font-bold md:text-5xl md:text-left md:leading-tight font-primary">
@@ -28,12 +30,12 @@ function Hero() {
           <div className="buttons flex flex-col items-center space-y-2 md:flex-row md:space-x-6">
             <button
               onClick={routeChange2}
-              className="text-lg transition ease-in-out delay-150 bg-white rounded-full px-10 py-3 border-2 hover:bg-transparent hover:border-skin hover:bg-darkblue hover:text-skin md:mt-2 border-darkblue text-darkblue"
+              className="text-lg transition ease-in-out delay-150 bg-white rounded-xl px-10 py-3 border-2 hover:bg-transparent hover:border-skin hover:bg-veryDarkBlue hover:text-skin md:mt-2 border-veryDarkBlue text-veryDarkBlue"
             >
               Find Mentor
             </button>
             <button
-              className="text-lg transition ease delay-150 bg-darkblue text-white rounded-full px-9 py-3 border-2 hover:bg-white hover:text-darkblue hover:border-darkblue"
+              className="text-lg transition ease delay-150 bg-veryDarkBlue text-white rounded-xl px-9 py-3 border-2 hover:bg-white hover:text-veryDarkBlue hover:border-veryDarkBlue"
               onClick={routeChange1}
             >
               Become Mentor
