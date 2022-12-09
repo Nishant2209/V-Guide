1c64e0b55f41a8fd055d44c6ca77d96950c721f6
Nishant Kumar Singh <nishant220902@gmail.com>
2022-12-09 23:21:14 +0530

    changed the font size, aos type

--- src/Components/TypesOfMentor/TOM.jsx
+++ src/Components/TypesOfMentor/TOM.jsx
@@ -3,34 +3,34 @@ import "./TOM.css";
 
 function TOM() {
   return (
-    <div class="typeOfMentors flex flex-col space-y-8 p-10 m-8 border-[0.5px] border-[#E53F19] border-solid text-veryDarkBlue">
-      <div class="heading flex flex-col items-center space-y-4 w-full">
-        <h3 class="tomheading text-4xl md:text-5xl lg:text-6xl font-extrabold font-primary my-5">
+    <div className="typeOfMentors flex flex-col space-y-8 p-4 md:p-10 m-4 md:m-8 border-[0.5px] border-[#E53F19] border-solid text-veryDarkBlue">
+      <div className="heading flex flex-col items-center space-y-4 w-full">
+        <h3 className="tomheading text-4xl md:text-5xl lg:text-6xl font-extrabold my-5">
           Type of Mentors
         </h3>
-        <p class=" font-bold text-md lg:text-xl md:w-2/6 text-center text-lg font-secondary my-5 leading-9">
+        <p className="font-bold text-md lg:text-xl px-4 md:w-2/3 lg:w-1/3 text-center my-5 leading-9">
           Different type of mentors are available to provide guidance to
           different level of projects.
         </p>
       </div>
-      <div class="mentordefination flex flex-col space-y-10 w-full px-6">
+      <div className="mentordefination flex flex-col space-y-10 w-full px-6">
         <div
-          class="defbox alpha flex flex-col md:flex-row space-y-4 lg:space-y-0 items-center self-start justify-around p-5 md:w-[48%] bg-[#d9d9d912] rounded-lg text-center lg:text-left"
-          data-aos="fade-right"
+          className="defbox alpha flex flex-col md:flex-row space-y-4 lg:space-y-0 items-center self-start justify-around p-5 md:w-[48%] bg-[#d9d9d912] rounded-lg text-center lg:text-left"
+          data-aos="zoom-out-down"
         >
-          <img src="./images/alpha-icon 1.png" alt="" class="h-24" />
-          <p class="text-md lg:text-lg md:w-3/5 font-semibold">
+          <img src="./images/alpha-icon 1.png" alt="" className="h-24" />
+          <p className="text-md lg:text-lg md:w-3/5">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
             labore, modi, numquam tempora ducimus necessitatibus molestiae
             assumenda dicta mollitia voluptatibus ipsam aliquam, et laborum.
           </p>
         </div>
         <div
-          class="defbox beta flex flex-col self-end md:flex-row space-y-4 lg:space-y-0 items-center justify-around p-5 md:w-[48%] bg-[#d9d9d912] rounded-lg text-center lg:text-left"
-          data-aos="fade-left"
+          className="defbox beta flex flex-col self-end md:flex-row space-y-4 lg:space-y-0 items-center justify-around p-5 md:w-[48%] bg-[#d9d9d912] rounded-lg text-center lg:text-left"
+          data-aos="zoom-out-down"
         >
-          <img src="./images/alpha-icon 1.png" alt="" class="h-24" />
-          <p class="text-md lg:text-lg md:w-3/5 font-semibold">
+          <img src="./images/alpha-icon 1.png" alt="" className="h-24" />
+          <p className="text-md lg:text-lg md:w-3/5">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit.
             Necessitatibus doloribus, architecto numquam nobis eveniet
             reprehenderit cum dicta nostrum neque? Quas ipsam dignissimos magnam
@@ -38,11 +38,11 @@ function TOM() {
           </p>
         </div>
         <div
-          class="defbox gama flex flex-col md:flex-row space-y-4 lg:space-y-0 items-center justify-around p-5 md:w-[48%] bg-[#d9d9d912] rounded-lg text-center lg:text-left"
-          data-aos="fade-right"
+          className="defbox gama flex flex-col md:flex-row space-y-4 lg:space-y-0 items-center justify-around p-5 md:w-[48%] bg-[#d9d9d912] rounded-lg text-center lg:text-left"
+          data-aos="zoom-out-down"
         >
-          <img src="./images/alpha-icon 1.png" alt="" class="h-24" />
-          <p class="text-md lg:text-lg md:w-3/5 font-semibold">
+          <img src="./images/alpha-icon 1.png" alt="" className="h-24" />
+          <p className="text-md lg:text-lg md:w-3/5">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
             iste, dolor vel ad, error quaerat reiciendis iusto ex repudiandae
             quia explicabo a aspernatur consequuntur?
