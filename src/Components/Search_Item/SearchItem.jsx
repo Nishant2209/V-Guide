52538ed39754b47b296268b1f5fd94550b28576c
Nishant Kumar Singh <nishant220902@gmail.com>
2023-01-15 19:40:19 +0530

    removed mentor's array and added functionality to see availability button

--- src/Components/Search_Item/SearchItem.jsx
+++ src/Components/Search_Item/SearchItem.jsx
@@ -1,95 +1,38 @@
 import "./searchItem.css";
+import { useState } from "react";
+import mentorsData from "../../mentors.json";
 
 const SearchItem = () => {
-  const mentors = [
-    {
-      img: {
-        source:
-          "https://cdn.fansshare.com/image/chrisevans/chris-evans-sharpened-square-wx-mxaneg-chris-evans-1926005607.jpg",
-        alt: "Chris",
-      },
-      name: "Chris",
-      branch: "BTech IT",
-      domain: "Web Development",
-      email: "thomas2905@gmail.com",
-      type: "./images/alpha-icon.png",
-    },
-    {
-      img: {
-        source:
-          "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw3NjA4Mjc3NHx8ZW58MHx8fHw%3D&w=1000&q=80",
-        alt: "Lewis",
-      },
-      name: "Lewis",
-      branch: "BTech IT",
-      domain: "App Development",
-      email: "lewis2209@gmail.com",
-      type: "./images/gamma-icon.png",
-    },
-    {
-      img: {
-        source:
-          "https://launchpad.syr.edu/wp-content/uploads/2021/11/Natasha-Brao-cropped-square.jpg",
-        alt: "Natasha",
-      },
-      name: "Natasha",
-      branch: "BTech IT",
-      domain: "Web Development",
-      email: "king123@rediffmail.com",
-      type: "./images/beta-icon.png",
-    },
-    {
-      img: {
-        source:
-          "https://www.byrdie.com/thmb/kCqE3NHA0vO2CnD4hCF7traxQX8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-8878126241-81ea4aa0cb934fcb95c0fe220633a2e0.jpg",
-        alt: "Ruth",
-      },
-      name: "Ruth J. Dyer",
-      branch: "BTech CSE",
-      domain: "App Development",
-      email: "ruth6969@gmail.com",
-      type: "./images/alpha-icon.png",
-    },
-    {
-      img: {
-        source:
-          "https://stylesatlife.com/wp-content/uploads/2019/09/Edgy-Side-Look-for-Men.jpg.webp",
-        alt: "Davis",
-      },
-      name: "Davis",
-      branch: "BTech IT",
-      domain: "Blockchain",
-      email: "davisscott23@gmail.com",
-      type: "./images/alpha-icon.png",
-    },
-    {
-      img: {
-        source:
-          "https://publish.purewow.net/wp-content/uploads/sites/2/2020/11/haircuts-for-square-faces-natalie-portman.jpg?fit=728%2C921",
-        alt: "Helen",
-      },
-      name: "Helen",
-      branch: "BTech IT",
-      domain: "Web Development",
-      email: "helen001@gmail.com",
-      type: "./images/gamma-icon.png",
-    },
-    {
-      img: {
-        source:
-          "http://cdn.shopify.com/s/files/1/0271/3101/9402/products/0_BEYONDSTAR-2019-Iron-Man-Tony-Stark-Square-Sunglasses-For-Men-Style-Leopard-Transparent-Lens-Sunglass-Women.jpg?v=1583504897",
-        alt: "Tony",
-      },
-      name: "Tony Stark",
-      branch: "BTech IT",
-      domain: "IOS Development",
-      email: "tony@gmail.com",
-      type: "./images/beta-icon.png",
-    },
-  ];
+  const [mentors, setMentors] = useState(mentorsData);
+
+  const handleAvailabilityClick = (element, hide) => {
+    // setAvailaibility("hidden");
+    // setSlots("flex");
+    setMentors(
+      mentors.map((e) =>
+        e.id === element.id
+          ? {
+              id: e.id,
+              img: e.img,
+              name: e.name,
+              branch: e.branch,
+              domain: e.domain,
+              email: e.email,
+              type: e.type,
+              showAvailaibility: hide ? false : true,
+              availaibility: e.availaibility,
+            }
+          : e
+      )
+    );
+  };
+
   return mentors.map((element) => {
     return (
-      <div className="searchItem p-5 rounded-md flex flex-col m-10 md:mb-5 md:mx-0 md:my-0 md:flex-row items-center justify-between border-2 border-veryDarkBlue gap-5 bg-gradient-to-r from-[#b4b7cd] via-white to-[#f0d0cb]" data-aos="fade-in">
+      <div
+        className="searchItem p-5 rounded-md flex flex-col m-10 md:mb-5 md:mx-0 md:my-0 md:flex-row items-center justify-between border-2 border-veryDarkBlue gap-5 bg-gradient-to-r from-[#b4b7cd] via-white to-[#f0d0cb]"
+        data-aos="fade-in"
+      >
         <img
           src={`${element.img.source}`}
           alt={`${element.img.alt}`}
@@ -114,10 +57,28 @@ const SearchItem = () => {
           </span>
         </div>
         <div className="siDetails flex flex-col justify-between md:self-start">
-          <div className="siDetailTexts text-right flex flex-col gap-1">
-            <button className="siCheckButton border-2 py-1 px-4 ml-3 border-veryDarkBlue rounded-lg font-bold backdrop-blur-xl bg-transparent shadow text-veryDarkBlue hover:scale-110 transition-all ease-in-out">
+          <div
+            className={`siDetailTexts text-right flex flex-col gap-1 transition-all ease-in-out 3000ms`}
+            onClick={() => handleAvailabilityClick(element)}
+          >
+            <button className="siCheckButton absolute border-2 py-1 px-4 border-veryDarkBlue rounded-lg font-bold backdrop-blur-xl bg-transparent shadow text-veryDarkBlue">
               See availability
             </button>
+          </div>
+          <div
+            className={`text-veryDarkBlue text-base md:text-lg font-semibold border-2 border-veryDarkBlue rounded-lg ${
+              element.showAvailaibility ? "flex" : "hidden"
+            } px-4 flex-col backdrop-blur-xl bg-transparent shadow pb-5 gap-2`}
+          >
+            <div
+              onClick={() => handleAvailabilityClick(element, true)}
+              className="cursor-pointer self-end -mr-2 md:mr-5 mt-2 md:mt-3"
+            >
+              <img src="./images/cross.png" alt="cross" className="w-8" />
+            </div>
+            <div>{element.availaibility.Slot_1}</div>
+            <div>{element.availaibility.Slot_2}</div>
+            <div>{element.availaibility.Slot_3}</div>
           </div>
         </div>
       </div>
