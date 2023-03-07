cf599141dd11e6a5e0f1834323c294cec825419a
Nishant Kumar Singh <nishant220902@gmail.com>
2023-03-08 00:42:13 +0530

    Made profile page responsive

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -43,55 +43,68 @@ const Profile = () => {
   }, []);
 
   return (
-    <div className="flex flex-row  h-[100vh]">
-      <div className="w-[25%] bg-orange items-center flex flex-col gap-y-4">
+    <div className="flex flex-col md:flex-row">
+      <div className="md:w-[25%] bg-orange items-center flex flex-col gap-y-4">
         <img
           src={
             user.gender === "Male" ? "./images/man.jpg" : "./images/woman.jpg"
           }
           alt="Woman"
-          className="w-48 rounded-full mt-16"
+          className="w-24 md:w-48 rounded-full mt-8 md:mt-16"
         />
-        <div className="text-2xl lg:text-3xl font-extrabold text-skin font-primary mb-2">
+        <div className="text-2xl md:text-3xl font-extrabold text-skin font-primary text-center">
           {user.name}
         </div>
-        <div className="text-lg lg:text-xl font-bold  border border-orange inline px-3 py-[1px] rounded-lg">
+        <div className="text-lg md:text-xl font-bold  border border-orange inline px-3 py-[1px] rounded-lg">
           {user.type}
         </div>
         {user.type === "Mentor" ? (
-          <div className="text-left font-[Pacifico] text-lg lg:text-xl mb-3 text-skin">
+          <div className="text-left font-[Pacifico] text-lg md:text-xl text-skin">
             {user.level}
           </div>
         ) : (
           ""
         )}
-        <div className="text-xl font-bold">{user.domain}</div>
+        <div className="text-xl font-bold mb-6 md:mb-0">{user.domain}</div>
       </div>
-      <div className="w-[75%] flex flex-col gap-y-6 text-lg ml-5 mt-2">
-        <div className="flex flex-col gap-y-4">
-          <div className="font-bold text-3xl">Information</div>
-          <hr className="bg-veryDarkBlue w-96 h-[2px]" />
-          <div>Full Name: {user.name}</div>
-          <div>Email: {user.email}</div>
-          <div>Phone: {user.phone}</div>
-          <div>Gender: {user.gender}</div>
+      <div className="md:w-[75%] flex flex-col gap-y-6 text-lg mx-5 my-4">
+        <div className="flex flex-col gap-y-4 text-base">
+          <div className="font-bold text-2xl md:text-3xl">Information</div>
+          <hr className="bg-veryDarkBlue w-[80%] md:w-96 h-[2px] -mt-2" />
+          <div>
+            <span className="font-bold">Full Name: </span>
+            {user.name}
+          </div>
+          <div>
+            <span className="font-bold">Email: </span> {user.email}
+          </div>
+          <div>
+            <span className="font-bold">Phone: </span>
+            {user.phone}
+          </div>
+          <div>
+            <span className="font-bold">Gender: </span>
+            {user.gender}
+          </div>
         </div>
         {user.type === "Mentor" ? (
           <div className="flex flex-col gap-y-4 w-full">
-            <div className="font-bold text-3xl">Enter your Availability</div>
-            <hr className="bg-veryDarkBlue w-96 h-[2.5px]" />
+            <div className="font-bold text-2xl md:text-3xl">
+              Enter your Availability
+            </div>
+            <hr className="bg-veryDarkBlue w-[80%] md:w-96 h-[2.5px] -mt-2" />
             <form
               onSubmit={handleSubmit}
-              className="flex flex-col gap-y-1 items-start"
+              className="flex md:flex-col flex-wrap flex-row gap-y-8 items-start text-base"
             >
-              <div>
-                <span className="mr-3">Slot 1: </span>
+              <div className="flex flex-col md:flex-row flex-wrap items-start w-full md:block">
+                <span className="md:mr-3">Slot 1: </span>
                 <select
                   name="day1"
                   id="day1"
                   onChange={onChange}
                   value={availability.day1}
-                  className="mr-3 mt-8 pl-2 w-32 xl:w-40 py-2 border-veryDarkBlue border-2 bg-transparent text-veryDarkBlue placeholder-veryDarkBlue shadow-xl"
+                  className="mr-3 md:mt-8 pl-2 w-full md:w-40 py-2 border-veryDarkBlue border-2 bg-transparent text-veryDarkBlue placeholder-veryDarkBlue shadow-xl"
                 >
                   <option value="">Day</option>
                   <option value="Monday">Monday</option>
@@ -109,7 +122,7 @@ const Profile = () => {
                   id="from1"
                   onChange={onChange}
                   value={availability.from1}
-                  className="mr-3 mt-8 pl-2 w-32 xl:w-40 py-2 border-veryDarkBlue border-2 bg-transparent text-veryDarkBlue placeholder-veryDarkBlue shadow-xl"
+                  className="mr-3 md:mt-8 pl-2 w-full md:w-40 py-2 border-veryDarkBlue border-2 bg-transparent text-veryDarkBlue placeholder-veryDarkBlue shadow-xl"
                 />
                 <span className="mr-3">To</span>
                 <input
@@ -118,17 +131,17 @@ const Profile = () => {
                   id="to1"
                   onChange={onChange}
                   value={availability.to1}
-                  className="mr-3 mt-8 pl-2 w-32 xl:w-40 py-2 border-veryDarkBlue border-2 bg-transparent text-veryDarkBlue placeholder-veryDarkBlue shadow-xl"
+                  className="mr-3 md:mt-8 pl-2 w-full md:w-40 py-2 border-veryDarkBlue border-2 bg-transparent text-veryDarkBlue placeholder-veryDarkBlue shadow-xl"
                 />
               </div>
-              <div>
-                <span className="mr-3">Slot 2: </span>
+              <div className="flex flex-col md:flex-row flex-wrap items-start w-full md:block">
+                <span className="md:mr-3">Slot 2: </span>
                 <select
                   name="day2"
                   id="day2"
                   onChange={onChange}
                   value={availability.day2}
-                  className="mr-3 mt-8 pl-2 w-32 xl:w-40 py-2 border-veryDarkBlue border-2 bg-transparent text-veryDarkBlue placeholder-veryDarkBlue shadow-xl"
+                  className="mr-3 md:mt-8 pl-2 w-full md:w-40 py-2 border-veryDarkBlue border-2 bg-transparent text-veryDarkBlue placeholder-veryDarkBlue shadow-xl"
                 >
                   <option value="">Day</option>
                   <option value="Monday">Monday</option>
@@ -146,7 +159,7 @@ const Profile = () => {
                   id="from2"
                   onChange={onChange}
                   value={availability.from2}
-                  className="mr-3 mt-8 pl-2 w-32 xl:w-40 py-2 border-veryDarkBlue border-2 bg-transparent text-veryDarkBlue placeholder-veryDarkBlue shadow-xl"
+                  className="mr-3 md:mt-8 pl-2 w-full md:w-40 py-2 border-veryDarkBlue border-2 bg-transparent text-veryDarkBlue placeholder-veryDarkBlue shadow-xl"
                 />
                 <span className="mr-3">To</span>
                 <input
@@ -155,17 +168,17 @@ const Profile = () => {
                   id="to2"
                   onChange={onChange}
                   value={availability.to2}
-                  className="mr-3 mt-8 pl-2 w-32 xl:w-40 py-2 border-veryDarkBlue border-2 bg-transparent text-veryDarkBlue placeholder-veryDarkBlue shadow-xl"
+                  className="mr-3 md:mt-8 pl-2 w-full md:w-40 py-2 border-veryDarkBlue border-2 bg-transparent text-veryDarkBlue placeholder-veryDarkBlue shadow-xl"
                 />
               </div>
-              <div>
-                <span className="mr-3">Slot 3: </span>
+              <div className="flex flex-col md:flex-row flex-wrap items-start w-full md:block">
+                <span className="md:mr-3">Slot 3: </span>
                 <select
                   name="day3"
                   id="day3"
                   onChange={onChange}
                   value={availability.day3}
-                  className="mr-3 mt-8 pl-2 w-32 xl:w-40 py-2 border-veryDarkBlue border-2 bg-transparent text-veryDarkBlue placeholder-veryDarkBlue shadow-xl"
+                  className="mr-3 md:mt-8 pl-2 w-full md:w-40 py-2 border-veryDarkBlue border-2 bg-transparent text-veryDarkBlue placeholder-veryDarkBlue shadow-xl"
                 >
                   <option value="">Day</option>
                   <option value="Monday">Monday</option>
@@ -183,7 +196,7 @@ const Profile = () => {
                   id="from3"
                   onChange={onChange}
                   value={availability.from3}
-                  className="mr-3 mt-8 pl-2 w-32 xl:w-40 py-2 border-veryDarkBlue border-2 bg-transparent text-veryDarkBlue placeholder-veryDarkBlue shadow-xl"
+                  className="mr-3 md:mt-8 pl-2 w-full md:w-40 py-2 border-veryDarkBlue border-2 bg-transparent text-veryDarkBlue placeholder-veryDarkBlue shadow-xl"
                 />
                 <span className="mr-3">To</span>
                 <input
@@ -192,12 +205,12 @@ const Profile = () => {
                   id="to3"
                   onChange={onChange}
                   value={availability.to3}
-                  className="mr-3 mt-8 pl-2 w-32 xl:w-40 py-2 border-veryDarkBlue border-2 bg-transparent text-veryDarkBlue placeholder-veryDarkBlue shadow-xl"
+                  className="mr-3 md:mt-8 pl-2 w-full md:w-40 py-2 border-veryDarkBlue border-2 bg-transparent text-veryDarkBlue placeholder-veryDarkBlue shadow-xl"
                 />
               </div>
               <button
                 type="submit"
-                className="mt-8 bg-veryDarkBlue text-skin px-5 py-1 text-lg hover:scale-110 transition-all duration-400 ease-in-out font-medium rounded-md hover:bg-transparent hover:border-2 hover:border-veryDarkBlue hover:text-veryDarkBlue"
+                className="md:mt-8 bg-veryDarkBlue text-skin px-5 py-1 text-lg hover:scale-110 transition-all duration-400 ease-in-out font-medium rounded-md hover:bg-transparent hover:border-2 hover:border-veryDarkBlue hover:text-veryDarkBlue"
               >
                 Save
               </button>
