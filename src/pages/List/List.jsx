fe95ab3973777e888596c790de989746d7489975
Nishant Kumar Singh <nishant220902@gmail.com>
2023-07-08 12:14:13 +0530

    not showing mentors page to mentors

--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -2,15 +2,27 @@ import "./list.css";
 import Header from "../../Components/Header/Header";
 import SearchItem from "../../Components/Search_Item/SearchItem";
 import Footer from "../../Components/Footer/Footer";
+import destinationContext from "../../Context/destinationContext";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
 
 const List = () => {
+  const navigate = useNavigate();
+  const context = useContext(destinationContext);
+  const { user, fetchUser } = context;
+  useEffect(() => {
+    fetchUser();
+    //eslint-disable-next-line
+  }, []);
+
   return (
     <div>
       <Header />
       <div className="listContainer">
         <div className="listWrapper">
           <div className="listResult">
-            <SearchItem />
+            {user.type === "Mentor" ? navigate("/Dashboard") : <SearchItem />}
           </div>
         </div>
       </div>
