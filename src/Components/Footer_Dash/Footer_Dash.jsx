c35124b990a769a2a1cb1c7ae18c3eaea2619bac
Nishant Kumar Singh <nishant220902@gmail.com>
2023-09-21 14:53:02 +0530

    removed the name

--- src/Components/Footer_Dash/Footer_Dash.jsx
+++ src/Components/Footer_Dash/Footer_Dash.jsx
@@ -19,10 +19,10 @@ function Footer_Dash() {
               </p>
             </div>
             <div class="neo flex flex-col w-1/2 md:h-full md:justify-end md:items-end">
-              <p class="md:text-lg font-normal text-white">Made with ❤️ by</p>
+              {/* <p class="md:text-lg font-normal text-white">Made with ❤️ by</p>
               <h2 class="text-2xl md:text-3xl md:my-1 font-bold text-[#E53F19] font-primary">
                 The Neophytes
-              </h2>
+              </h2> */}
             </div>
           </div>
         </div>
